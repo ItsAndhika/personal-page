a76c3ed0ce05fa3806bc497e8b22bd39ce75779a
ItsAndhika <bambangindaryanto12@gmail.com>
2023-01-29 12:19:15 +0800

    Done but some feature is still incomplete and there are some bugs

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,13 @@
+window.onscroll = function() {
+    const header = document.querySelector("header")
+    const fixedNav = header.offsetTop
+
+    if(window.pageYOffset > fixedNav) {
+        header.classList.add("navbar")
+    } else {
+        header.classList.remove("navbar")
+    }
+}
 const menu = document.getElementById('menu')
 const nav = document.getElementById('nav')
 menu.addEventListener('click', function() {
