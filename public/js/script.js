469e7f809135434dff19ad9295e63ee1640d3e39
ItsAndhika <bambangindaryanto12@gmail.com>
2023-03-16 18:50:13 +0800

    Make the contact form works

--- public/js/script.js
+++ public/js/script.js
@@ -26,6 +26,21 @@ menu.addEventListener('click', function() {
     menu.lastElementChild.classList.toggle('lastSpan')
 })
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxk5wFU7JdMUY-BoHAQeMZQ9gp3dl0U2-gMl0cdm1AYlC-Pf642oRXti-hwKmThRDs/exec'
+const form = document.forms['contact-form']
+const sendButton = document.getElementById('send')
+const alert = document.getElementById('alert')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        alert.classList.toggle('hidden')
+        form.reset()
+        console.log('Success!', response)
+    })
+    .catch(error => console.error('Error!', error.message))
+})
 
 const text = document.getElementById('job')
 const textLoad = () => {
